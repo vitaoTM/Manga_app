ffe5904afc9212d5f97bacafd49db676f05f760b
vitaotm <vitor.grosskopf@gmail.com>
2026-03-12 20:57:22 -0300

    feat(ui): add avatar upload template and refine manga navigation
    Enhance the user profile by adding a reusable avatar upload template
    and configuring multipart form support for profile updates. Also,
    correct manga navigation links and add a route for removing user
    avatars.
    
    - Create app/views/devise/registrations/avatar_template.html.erb.
    - Update registration edit view to support multipart forms and remove
      redundant name fields.
    - Update manga index and new views with correct navigation links.
    - Add delete route for user avatars in config/routes.rb.
    - Reorder routes for better organization.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "mangas#index"
 
+  devise_for :users
+    delete "users/avatar", to: "users/avatars#destroy", as: :remove_avatar
   resources :mangas, only: [ :index, :show, :edit, :new, :create, :update, :destroy ]
 end
