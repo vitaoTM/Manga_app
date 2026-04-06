29c4138633df01b780e50099bb3f6a4a226b1eca
vitaotm <vitor.grosskopf@gmail.com>
2026-04-06 15:04:00 -0300

    Update stripe initializers

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,2 @@
 require "stripe"
 Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
-Rails.logger.info "Stripe key loaded: #{Stripe.api_key.present?}"
-Rails.logger.info "Webhook secret loaded: #{ENV['STRIPE_WEBHOOK_SECRET'].present?}"
